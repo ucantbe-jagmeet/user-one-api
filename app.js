8f645a3dc080160707dba044d1f24d7265ce53b6
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-01 04:14:47 +0530

    Put method completed

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.get("/api/v1/user", (req, res) => {
 
 app.post("/api/v1/user", (req, res) => {
   const { id, name } = req.body;
-
   const newUser = { id: id, name: name };
   const updatedUser = [...people, newUser];
 
@@ -20,9 +19,9 @@ app.post("/api/v1/user", (req, res) => {
       .status(400)
       .json({ success: false, msg: "please Provide name value" });
   }
-
   res.status(201).json({ success: true, newUser });
 });
+
 app.post("/api/v1/user/display", (req, res) => {
   const { id, name } = req.body;
 
@@ -31,10 +30,31 @@ app.post("/api/v1/user/display", (req, res) => {
       .status(400)
       .json({ success: false, msg: "please Provide name value" });
   }
-
   res
     .status(201)
     .json({ success: true, data: [...people, { id: id, name: name }] });
+});
+
+app.put("/api/v1/user/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const user = people.find((user) => user.id === Number(id));
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `user doesn't exist with this id:${id}` });
+  }
+
+  const newUser = people.map((user) => {
+    if (user.id === Number(id)) {
+      user.name = name;
+    }
+    return user;
+  });
+
+  res.status(200).json({ success: true, data: newUser });
 });
 
 app.all("*", (req, res) => {
