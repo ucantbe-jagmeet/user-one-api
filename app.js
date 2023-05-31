fe00c15ded0e4fe9ac38ccf316403180f3883f0e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-01 02:18:44 +0530

    creating dummy data

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "First server" });
+  res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
 });
 
-app.listen(3000);
+app.get("/about", (req, res) => {
+  res.status(200).json([{ msg: "About page" }]);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
