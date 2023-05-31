bd12f889938cfab9744f513e901d4b83297bc0e5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-01 03:55:00 +0530

    Post method display

--- app.js
+++ app.js
@@ -5,24 +5,42 @@ let { people } = require("./data");
 // parse json here
 app.use(express.json());
 
-app.get("/api/user", (req, res) => {
+app.get("/api/v1/user", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
-app.post("/api/user", (req, res) => {
-  const { name } = req.body;
+app.post("/api/v1/user", (req, res) => {
+  const { id, name } = req.body;
+
+  const newUser = { id: id, name: name };
+  const updatedUser = [...people, newUser];
+
   if (!name) {
     return res
       .status(400)
       .json({ success: false, msg: "please Provide name value" });
   }
-  res.status(201).json({ success: true, user: name });
+
+  res.status(201).json({ success: true, newUser });
+});
+app.post("/api/v1/user/display", (req, res) => {
+  const { id, name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please Provide name value" });
+  }
+
+  res
+    .status(201)
+    .json({ success: true, data: [...people, { id: id, name: name }] });
 });
 
 app.all("*", (req, res) => {
   res.status(404).send("Resource not found");
 });
 
-app.listen(5000, () => {
-  console.log("Server is listening on port 5000...");
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
 });
--- data.js
+++ data.js
@@ -1,13 +1,9 @@
 const people = [
-  { id: 11, name: "peter1" },
+  { id: 1, name: "peter1" },
   { id: 2, name: "peter2" },
   { id: 3, name: "peter3" },
   { id: 4, name: "peter4" },
   { id: 5, name: "peter5" },
-  { id: 6, name: "peter6" },
-  { id: 7, name: "peter7" },
-  { id: 8, name: "peter8" },
-  { id: 9, name: "peter9" },
 ];
 
 module.exports = { people };
