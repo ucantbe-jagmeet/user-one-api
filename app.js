c5f03e0112358fafa36fe9d7628841fc0270240d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-01 16:58:01 +0530

    db connected

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
 const authRouter = require("./routes/authRouter");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // parse json here
 app.use(express.json());
@@ -13,6 +15,17 @@ app.all("*", (req, res) => {
   res.status(404).send("Resource not found");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
-});
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
