21bf7b3399a7b4a8ebd262eafbbd63c8573459a3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-01 15:15:58 +0530

    Setting up user router

--- app.js
+++ app.js
@@ -1,76 +1,18 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+const userRouter = require("./routes/userRouter");
 
 // parse json here
 app.use(express.json());
 
-app.get("/api/v1/user", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+app.use("/api/v1/user", userRouter);
 
-app.post("/api/v1/user", (req, res) => {
-  const { id, name } = req.body;
-  const newUser = { id: id, name: name };
-  const updatedUser = [...people, newUser];
-
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please Provide name value" });
+app.post("/login", (req, res) => {
+  const { name } = rq.body;
+  if (name) {
+    return res.status(200).send(`welcome ${name}`);
   }
-  res.status(201).json({ success: true, newUser });
-});
-
-app.post("/api/v1/user/display", (req, res) => {
-  const { id, name } = req.body;
-
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please Provide name value" });
-  }
-  res
-    .status(201)
-    .json({ success: true, data: [...people, { id: id, name: name }] });
-});
-
-app.put("/api/v1/user/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const user = people.find((user) => user.id === Number(id));
-
-  if (!user) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `user doesn't exist with this id:${id}` });
-  }
-
-  const newUser = people.map((user) => {
-    if (user.id === Number(id)) {
-      user.name = name;
-    }
-    return user;
-  });
-
-  res.status(200).json({ success: true, data: newUser });
-});
-
-app.delete("/api/v1/user/:id", (req, res) => {
-  const { id } = req.params;
-
-  const user = people.find((user) => user.id === Number(id));
-
-  if (!user) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `user doesn't exist with this id:${id}` });
-  }
-
-  const newUser = people.filter((user) => user.id !== Number(id));
-
-  res.status(200).json({ success: true, data: newUser });
+  res.status(401).send("Please provide credentials");
 });
 
 app.all("*", (req, res) => {
---
+++ routes/userRouter.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+let { people } = require("../data");
+
+router.get("/api/v1/user", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+router.post("/api/v1/user", (req, res) => {
+  const { id, name } = req.body;
+  const newUser = { id: id, name: name };
+  const updatedUser = [...people, newUser];
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please Provide name value" });
+  }
+  res.status(201).json({ success: true, newUser });
+});
+
+router.post("/api/v1/user/display", (req, res) => {
+  const { id, name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please Provide name value" });
+  }
+  res
+    .status(201)
+    .json({ success: true, data: [...people, { id: id, name: name }] });
+});
+
+router.put("/api/v1/user/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const user = people.find((user) => user.id === Number(id));
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `user doesn't exist with this id:${id}` });
+  }
+
+  const newUser = people.map((user) => {
+    if (user.id === Number(id)) {
+      user.name = name;
+    }
+    return user;
+  });
+
+  res.status(200).json({ success: true, data: newUser });
+});
+
+router.delete("/api/v1/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  const user = people.find((user) => user.id === Number(id));
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `user doesn't exist with this id:${id}` });
+  }
+
+  const newUser = people.filter((user) => user.id !== Number(id));
+
+  res.status(200).json({ success: true, data: newUser });
+});
+
+module.exports = router;
